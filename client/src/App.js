796b85fccf6ef0377b5d10c69e60e712c35c2fe5
ao-joker <ao-joker@users.noreply.github.com>
2022-02-22 18:33:28 -0500

    Implementing React

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">App</header>
+    </div>
+  );
+}
+
+export default App;
