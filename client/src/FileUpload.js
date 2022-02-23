48192a54fed51f59c2ea96c89d0f8309df5aece7
ao-joker <ao-joker@users.noreply.github.com>
2022-02-22 23:42:42 -0500

    Making the components necessary for a file Upload

--- client/src/FileUpload.js
+++ client/src/FileUpload.js
@@ -1,17 +1,52 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+import axios from 'axios';
 
 /*Here is the basis for the file upload mechanism
     - Firstly, the basic set up for the choosing file and then upload file system is placed in using the Fragment
+    - Secondly, properly take in the file, acknowlegde its existence, and know what to pass
 */
 function FileUpload()
 {
+    //Set up a bunch of states for the file upload
+    const [file, setFile] = useState()                      //Change the value in order to immediately and cleanly set the file
+    const [fileName, setFileName] = useState()              //Using this as part of a hook to note the type of file, the file itself, and what to send to submission
+    const [uploadedFile, setUploadedFile] = useState({})    //Obtain the returned file object
+
+    const onChange = function(e)
+                    {
+                        setFile(e.target.files[0])        
+                        setFileName(e.target.files[0].name)
+                    }
+
+    const onSubmit = async function(e)
+                    {
+                        e.preventDefault()
+
+                        //Create a form data to actually take in the file
+                        const formData = new formData()
+                        formData.append("file") //This links to the backend server component of req.files.file!
+
+                        //A try-catch in order to take in the file and place it in the right directory
+                        try
+                        {
+                            const res = await axios.post("/upload", formData, {headers: {"Content-Type": "multipart/form-data"}})
+
+                            //Now extract the fileName and path from the res
+                            const {fileName, filePath} = res.data
+                            setUploadedFile({fileName, filePath})
+                        }
+                        catch(err)
+                        {
+
+                        }
+                    }
+
     return(
         <Fragment>
-            <form>
+            <form onSubmit = {onSubmit}>
                 <div className = "input-group mb-3">
-                    <input type = "file" className = "form-control" id = "inputGroupFile02"></input>
+                    <input type = "file" className = "form-control" id = "customFile" onChange = {onChange}></input>
                     <label className = "custom-file-label" htmlFor = "custom-file"></label>
-
                     <input type = "submit" value = "Upload" className = "btn btn-primary btn-block"></input> 
                 </div>
             </form>
