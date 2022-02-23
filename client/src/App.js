54013d3ab8d9e96daaa5448ad8e8b0beefabe1fb
ao-joker <ao-joker@users.noreply.github.com>
2022-02-22 22:44:59 -0500

    Begin working on Panel B parts

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
 
 function App() {
+
+  //Some constant titles
+  const acknowledgements = "BCB MQP Advisor: Dr. Lane Harrison  ;  BBT MQP Advisor: Dr. Scarlet Shell";
+
   return (
     <div className="App">
-      <header className="App-header">App</header>
+      <header className="container"></header>
+      <h1 className="text-center"><i>Mycobacterium tuberculosis</i> Metabolic Pathway Biovisualization</h1>
+         <h3 className="text-center mt-4">An MQP completed by Adrian Orszulak</h3>
+         <h4 className="text-center mt-2">{acknowledgements}</h4>
+
     </div>
   );
 }
