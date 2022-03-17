6b160ac5e23b5c60213cf2518c30f4695635c169
ao-joker <ao-joker@users.noreply.github.com>
2022-03-17 14:30:31 -0400

    Changing some order

--- client/src/RegulationData.js
+++ client/src/RegulationData.js
@@ -1,23 +1,29 @@
 import React, {useEffect} from "react"
 import * as d3 from "d3";
 import {workingFile} from "./FileUpload"
+import {cleanedAndOrganizedData as masterData} from "./Home"
 
 const RegulationData = () =>
 {
 
     useEffect(() => 
     {
-        //Clear the whole svg for a new file being uploaded
-        d3.select("#Regulation").selectAll("svg > *").remove()
+        if(workingFile.filename !== undefined)
+        {
+            //Clear the whole svg for a new file being uploaded
+            d3.select("#Regulation").selectAll("svg > *").remove()
 
-        //Draw a new background and the tables of the information from that file
-        makeRegulationListBackground()
+            //Draw a new background and the tables of the information from that file
+            makeRegulationListBackground()
 
-        d3.csv().then(function(data)
-                    {
-                        //Variable that defines the columns of the csv, that being simply the protein name ("Rv####") and
-                        drawTables(data)
-                    })
+            d3.csv().then(function(data)
+                        {
+                            //Variable that defines the columns of the csv, that being simply the protein name ("Rv####") and fold change
+                            //Then, other details will be pulled from the existing masterArray dataset
+                            var columns = ["Trasncription Factor/Protein Name", "Pathway", "Regulation Type", "Fold Change"]
+                            drawTables(data, columns)
+                        })
+        }
 
         function makeRegulationListBackground()
         {
@@ -32,21 +38,16 @@ const RegulationData = () =>
       
         }
 
-        function drawTables(cleanedData)
+        function drawTables(data, columns)
         {
-            if(workingFile.filename !== undefined)
-            {
-                /*d3.select("#Regulation")
+            d3.select("#Regulation")
                 .append("text")
                 .attr("x", 200)
                 .attr("y", 200)
                 .attr("stroke", "black")
                 .attr("fill", "green")
                 .attr("font-size", 50)
-                .text(`${workingFile.filename}`)*/
-
-
-            }
+                .text(`${workingFile.filename}`)
         }
 
         function showRegulationChangesInPathway()
