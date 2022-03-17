e2a156be7bf0a1fa2d040350588c6df0565b1efc
ao-joker <ao-joker@users.noreply.github.com>
2022-03-17 13:51:15 -0400

    Now, the file upload and data table creation mechanism should work

--- client/src/RegulationData.js
+++ client/src/RegulationData.js
@@ -7,28 +7,60 @@ const RegulationData = () =>
 
     useEffect(() => 
     {
-        if(workingFile.filename !== undefined)
+        //Clear the whole svg for a new file being uploaded
+        d3.select("#Regulation").selectAll("svg > *").remove()
+
+        //Draw a new background and the tables of the information from that file
+        makeRegulationListBackground()
+
+        d3.csv().then(function(data)
+                    {
+                        drawTables(data)
+                    })
+
+        function makeRegulationListBackground()
         {
             d3.select("#Regulation")
             .append("rect")
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("width", 100)
-            .attr("height", 100)
-            .attr("stroke", "black")
-            .attr("fill", "green")
-
-            d3.select("#Regulation")
-            .append("text")
-            .attr("x", 200)
-            .attr("y", 200)
-            .attr("stroke", "black")
-            .attr("fill", "green")
-            .attr("font-size", 50)
-            .text(`${workingFile.filename}`) 
-            
-            d3.csv()
+              .attr("x", 0)
+              .attr("y", 0)
+              .attr("width", 1400)
+              .attr("height", 1000)
+              .attr("stroke", "blue")
+              .attr("fill", "blue")
+      
         }
+        function drawTables()
+        {
+            if(workingFile.filename !== undefined)
+            {
+                d3.select("#Regulation")
+                .append("rect")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("width", 100)
+                .attr("height", 100)
+                .attr("stroke", "black")
+                .attr("fill", "green")
+
+                d3.select("#Regulation")
+                .append("text")
+                .attr("x", 200)
+                .attr("y", 200)
+                .attr("stroke", "black")
+                .attr("fill", "green")
+                .attr("font-size", 50)
+                .text(`${workingFile.filename}`) 
+                
+                d3.csv()
+            }
+        }
+
+        function showRegulationChangesInPathway()
+        {
+
+        }
+
 
     })
 
